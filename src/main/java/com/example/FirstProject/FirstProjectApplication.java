6abbf7628bbce1d1ae325b66652237994d15e119
abdallahabadallahabdalla <abdallah.bakr@student.guc.edu.eg>
2026-03-31 15:43:10 +0200

    fixing bugs

--- src/main/java/com/example/FirstProject/FirstProjectApplication.java
+++ src/main/java/com/example/FirstProject/FirstProjectApplication.java
@@ -2,10 +2,9 @@ package com.example.FirstProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-
-
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class FirstProjectApplication {
 
 	public static void main(String[] args) {
